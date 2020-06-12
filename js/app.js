3d1082c2e834fc9b1d837655c2d7b93803e14ef5
Leo927 <asd347925290@gmail.com>
2020-06-11 22:48:22 -0600

    refactored to use function

--- js/app.js
+++ js/app.js
@@ -1,23 +1,45 @@
 $(document).ready(function(){
-	for (var i = 1; i <= 100; i++) {
+	
 
-		var output;
+	var convertedValues = fizzBuzz(getUserInput());
+
+	convertedValues.forEach(x=>$("ul").append("<li>"+x+"</li>"));
+});
+
+function fizzBuzz(values)
+{
+	var result =[];
+	for (var i = 1; i <= values; i++) {
+
+		var temp;
 		if(i%3==0 && i%5==0)
 		{
-			output = "Fizz Buzz";
+			temp = "Fizz Buzz";
 		}
 		else if(i%3==0)
 		{
-			output = "Fizz";
+			temp = "Fizz";
 		}
 		else if(i%5==0)
 		{
-			output = "Buzz";
+			temp = "Buzz";
 		}
 		else
 		{
-			output = i;
+			temp = i;
 		}
-		$("ul").append("<li>"+output+"</li>");
+		result.push(temp);
 	}
-})
+	return result;
+}
+
+function getUserInput()
+{
+	var num = prompt("Please enter an integar");
+	while(!parseInt(num) || +num%1!=0)
+	{
+		var num = prompt("Please enter a integar");
+	}
+	num = +num;
+	return num;
+}
